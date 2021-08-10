cc5417c71619acb254239c0067dfdb9bac453db2
Jayson Kenn Julian <jkpjulian@icreatedev.live>
2021-08-10 19:08:28 +0800

    initial

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,10 +5,10 @@
         </div>
         <div class="justify-end hidden lg:block">
             <div class="flex flex-row items-center justify-center w-full h-full space-x-6 font-digitalo">
-                <a href="{{ url('/websites') }}" class="text-sm font-medium text-gray-900 uppercase lg:text-md hover:text-sky-600">
+                <a href="{{ url('/websites') }}" class="text-sm font-medium uppercase lg:text-md hover:text-sky-600 {{ request()->is('websites') ? 'text-sky-900' : 'text-gray-900' }}">
                     websites
                 </a>
-                <a href="{{ url('/games') }}" class="text-sm font-medium text-gray-900 uppercase lg:text-md hover:text-sky-600">
+                <a href="{{ url('/games') }}" class="text-sm font-medium uppercase lg:text-md hover:text-sky-600 {{ request()->is('games') ? 'text-sky-900' : 'text-gray-900' }}">
                     games
                 </a>
                 {{-- <a href="{{ url('/marketing') }}" class="text-sm font-medium text-gray-900 uppercase lg:text-md hover:text-sky-600">
@@ -17,10 +17,10 @@
                 {{-- <a href="{{ url('/events') }}" class="text-sm font-medium text-gray-900 uppercase lg:text-md hover:text-sky-600">
                     events
                 </a> --}}
-                <a href="{{ url('/about') }}" class="text-sm font-medium text-gray-900 uppercase lg:text-md hover:text-sky-600">
+                <a href="{{ url('/about') }}" class="text-sm font-medium uppercase lg:text-md hover:text-sky-600 {{ request()->is('about') ? 'text-sky-900' : 'text-gray-900' }}">
                     our company
                 </a>
-                <a href="{{ url('/contact') }}" class="text-sm font-medium text-gray-900 uppercase lg:text-md hover:text-sky-600">
+                <a href="{{ url('/contact') }}" class="text-sm font-medium uppercase lg:text-md hover:text-sky-600 {{ request()->is('contact') ? 'text-sky-900' : 'text-gray-900' }}">
                     contact us
                 </a>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -19,18 +19,18 @@ Route::get('/', function () {
 
 Route::get('/websites', function () {
     return view('website');
-});
+})->name('websites');
 
 Route::get('/games', function () {
     return view('game');
-});
+})->name('games');
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
 
 Route::get('/contact', function () {
     return view('contact');
-});
+})->name('contact');
 
 Route::post('/inquiry', 'App\Http\Controllers\ContactController@inquiry')->name('inquiry');
